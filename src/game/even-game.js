1b9ac129e4a95fe2fe312cb54cb562d68406a01a
andrew_totmin <andrew.totmin@gmail.com>
2024-06-08 18:07:17 +0500

    Revert "Исправил файл с логикой игры even-game.js"
    This reverts commit 2479cca4a6b1657e111d5173374876f2b7db650f.

--- src/game/even-game.js
+++ src/game/even-game.js
@@ -4,29 +4,44 @@ export default () => {
   greeting();
 
   const userName = getName();
-  let correctAnswer;
   let i = 0;
 
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   console.log('--------');
 
-  while (i < 3) {
+  do {
     const question = getRandomInt(1, 50);
     console.log(`Question: ${question}`);
 
     const userAnswer = getAnswer();
 
-    question % 2 === 0 ? (correctAnswer = 'yes') : (correctAnswer = 'no');
+    if ((userAnswer !== 'yes') && (userAnswer !== 'no')) {
+      console.log(`"${userAnswer}" is wrong answer.`);
+      console.log('Correct answer "yes" if the number is even, otherwise answer "no".');
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
 
-    if (correctAnswer === userAnswer) {
+    if ((question % 2 === 0) && (userAnswer === 'yes')) {
       console.log('Correct!');
       console.log('--------');
       i += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    }
+    if ((question % 2 !== 0) && (userAnswer === 'yes')) {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+    if ((question % 2 !== 0) && (userAnswer === 'no')) {
+      console.log('Correct!');
+      console.log('--------');
+      i += 1;
+    }
+    if ((question % 2 === 0) && (userAnswer === 'no')) {
+      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
       console.log(`Let's try again, ${userName}!`);
       break;
     }
     if (i === 3) console.log(`Congratulations, ${userName}!`);
-  }
+  } while (i < 3);
 };
