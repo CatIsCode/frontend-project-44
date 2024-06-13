3cbdbf7c8abc66c78f3cc6ff7ddc77033f873ec9
andrew_totmin <andrew.totmin@gmail.com>
2024-06-13 22:45:59 +0500

    Исправил ошибки указанные в код ревью #1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import readlineSync from 'readline-sync';
 
-export default () => console.log('Welcome to the Brain Games!');
-
-export const getName = () => {
+const getName = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   return name;
+};
+
+const separator = () => {
+  console.log('--------');
 };
 
 export const getAnswer = () => readlineSync.question('Your answer: ');
@@ -14,4 +16,29 @@ export const getRandomInt = (min, max) => {
   const minimum = Math.ceil(min);
   const maximum = Math.floor(max);
   return Math.floor(Math.random() * (maximum - minimum) + minimum);
+};
+
+export default (descr, getQA) => {
+  let i = 0;
+  console.log('Welcome to the Brain Games!');
+  const userName = getName();
+  console.log(descr);
+  separator();
+
+  while (i < 3) {
+    const [question, correctAnswer] = getQA();
+    console.log(`Question: ${question}`);
+    const userAnswer = getAnswer();
+
+    if (String(correctAnswer) === userAnswer) {
+      console.log('Correct!');
+      separator();
+      i += 1;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      break;
+    }
+    if (i === 3) console.log(`Congratulations, ${userName}!`);
+  }
 };
