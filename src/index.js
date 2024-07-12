de353733b867d68d7d03a34177811070cd39b87d
andrew_totmin <andrew.totmin@gmail.com>
2024-07-12 18:39:49 +0500

    fix code review #2

--- src/index.js
+++ src/index.js
@@ -1,44 +1,27 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
-
-const separator = () => {
-  console.log('--------');
-};
-
-export const getAnswer = () => readlineSync.question('Your answer: ');
-
-export const getRandomInt = (min, max) => {
-  const minimum = Math.ceil(min);
-  const maximum = Math.floor(max);
-  return Math.floor(Math.random() * (maximum - minimum) + minimum);
-};
-
-export default (descr, getQA) => {
+export default (descr, getQuestionAndAnswer) => {
   let i = 0;
   console.log('Welcome to the Brain Games!');
-  const userName = getName();
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log(descr);
-  separator();
+  console.log('--------');
 
   while (i < 3) {
-    const [question, correctAnswer] = getQA();
+    const [question, correctAnswer] = getQuestionAndAnswer();
     console.log(`Question: ${question}`);
-    const userAnswer = getAnswer();
+    const userAnswer = readlineSync.question('Your answer: ');
 
     if (String(correctAnswer) === userAnswer) {
       console.log('Correct!');
-      separator();
+      console.log('--------');
       i += 1;
     } else {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return;
     }
-    if (i === 3) console.log(`Congratulations, ${userName}!`);
   }
+  console.log(`Congratulations, ${userName}!`);
 };
